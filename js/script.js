9eddc26e60aa481c24d717f44bd953df4efe66dd
Mitja Kastelic <kastelic.mitja@gmail.com>
2014-05-06 13:49:01 +0200

    index updated for image viewer
    script eddited, index edited

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    /* skrbi za prikaz slike v novem oknu*/
+    $('a.grouped_elements').fancybox();
+    
     $('.more').click(function(){
         console.log("klik");
         $(this).css('background-color', 'red');;    
